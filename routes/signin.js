d5e4b3cc1790f1215d0afb44a17d91ddb8b6d557
yuki-I <j2209@matsue-ct.ac.jp>
2025-07-03 05:38:21 +0000

    yuuetsukann

--- routes/signin.js
+++ routes/signin.js
@@ -1,38 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const knex = require("../db/knex");
+const bcrypt = require("bcrypt");
+
+
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+ const isAuth = Boolean(userId);
+
   res.render("signin", {
-    title: "Sign in",
+    title: "Sign in",isAuth: isAuth,
   });
 });
 
 router.post('/', function (req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
+  const userId = req.session.userid;
+ const isAuth = Boolean(userId);
 
   knex("users")
     .where({
       name: username,
-      password: password,
+      
     })
     .select("*")
-    .then((results) => {
-      if (results.length === 0) {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-        });
-      } else {
-        req.session.userid = results[0].id;
-        res.redirect('/');
-      }
-    })
+    .then(async function (results) {
+  if (results.length === 0) {
+    res.render("signin", {
+      title: "Sign in",
+      errorMessage: ["ユーザが見つかりません"],
+      isAuth: isAuth,
+    });
+  } else if (await bcrypt.compare(password, results[0].password)) {
+    req.session.userid = results[0].id;
+    res.redirect('/');
+  } else {
+    res.render("signin", {
+      title: "Sign in",
+      errorMessage: ["ユーザが見つかりません"],
+      isAuth: isAuth,
+    });
+  }
+})
     .catch(function (err) {
       console.error(err);
       res.render("signin", {
-        title: "Sign in",
+        title: "Sign in",isAuth: isAuth,
         errorMessage: [err.sqlMessage],
       });
     });
