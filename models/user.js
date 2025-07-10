b57200244a173552d3e440d08656fe604b335568
yuki-I <j2209@matsue-ct.ac.jp>
2025-07-10 05:34:08 +0000

    19

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const knex = require("../db/knex");
+
+const TABLE_NAME = "users";
+
+async function findById(userId) {
+  const user = await where({id: userId});
+  if (user === null) {
+    throw new Error("User not found")
+  }
+  return {...user};
+}
+
+async function where(condition) {
+  return await knex(TABLE_NAME)
+    .where(condition)
+    .then((results) => {
+      if (results.length === 0) {
+        return null;
+      }
+      console.log(results[0]);
+      return results[0];
+    });
+}
+
+module.exports = {
+  findById,
+};
--- routes/signin.js
+++ routes/signin.js
@@ -1,56 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const knex = require("../db/knex");
-const bcrypt = require("bcrypt");
-
-
+const passport = require("passport");
 
 router.get('/', function (req, res, next) {
-  const userId = req.session.userid;
- const isAuth = Boolean(userId);
-
+  const isAuth = req.isAuthenticated();
   res.render("signin", {
-    title: "Sign in",isAuth: isAuth,
+    title: "Sign in",
+    isAuth: isAuth,
   });
 });
 
-router.post('/', function (req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  const userId = req.session.userid;
- const isAuth = Boolean(userId);
-
-  knex("users")
-    .where({
-      name: username,
-      
-    })
-    .select("*")
-    .then(async function (results) {
-  if (results.length === 0) {
-    res.render("signin", {
-      title: "Sign in",
-      errorMessage: ["ユーザが見つかりません"],
-      isAuth: isAuth,
-    });
-  } else if (await bcrypt.compare(password, results[0].password)) {
-    req.session.userid = results[0].id;
-    res.redirect('/');
-  } else {
-    res.render("signin", {
-      title: "Sign in",
-      errorMessage: ["ユーザが見つかりません"],
-      isAuth: isAuth,
-    });
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/signin',
+    failureFlash: true,
   }
-})
-    .catch(function (err) {
-      console.error(err);
-      res.render("signin", {
-        title: "Sign in",isAuth: isAuth,
-        errorMessage: [err.sqlMessage],
-      });
-    });
-});
+));
 
 module.exports = router;
